c0e7104f5e20d27609c62a949535c372e54c0e9c
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-05-20 18:58:38 -0300

    Update BHSIAI.js

--- 798326_Epsilon/BHSIAI.js
+++ 798326_Epsilon/BHSIAI.js
@@ -5,7 +5,6 @@ function refreshTime() {
   var dateString = new Date().toLocaleString("en-US", {timeZone: "America/Buenos_Aires"});
   var formattedString = dateString.replace(", ", " - ");
   timeDisplay.innerHTML = formattedString;
-  timeDisplay.requestFullscreen()
 }
 
 setInterval(refreshTime, 1000);

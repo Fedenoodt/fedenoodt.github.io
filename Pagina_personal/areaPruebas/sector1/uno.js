878f9d99abfbd2b4fba3d201ef9034a587bc5f2f
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-05 20:04:28 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -7,3 +7,14 @@ console.log("thisVar:", thisVar, "and thatVar:", thatVar);
 
 var greet = "Hello", who = "World";
 console.log("%s, %s!", greet, who);
+
+console.log({
+ 'Email': '',
+ 'Groups': {},
+ 'Id': 33,
+ 'IsHiddenInUI': false,
+ 'IsSiteAdmin': false,
+ 'LoginName': 'i:0#.w|virtualdomain\\user2',
+ 'PrincipalType': 1,
+ 'Title': 'user2'
+});

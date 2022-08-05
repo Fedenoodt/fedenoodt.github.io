b1149d2dc23c09c3895d89ed3efb9d27a50fa944
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-05 16:58:19 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -2,3 +2,7 @@ console.log("Hello, World!");
 
 var foo = "bar";
 console.log(foo);
+
+var thisVar = 'first value';
+var thatVar = 'second value';
+console.log("thisVar:", thisVar, "and thatVar:", thatVar);

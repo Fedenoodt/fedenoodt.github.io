87ee9f797439a06286aae5ccea534e69788f1def
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-08 17:52:31 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -37,3 +37,11 @@ console.log({
 }
 
 console.log(document.body);
+
+var canvas = document.createElement('canvas');
+canvas.width = 500;
+canvas.height = 250;
+var ctx = canvas.getContext('2d');
+ctx.font = '30px Cursive';
+ctx.fillText("Hello world!", 50, 50);
+document.body.appendChild(canvas);

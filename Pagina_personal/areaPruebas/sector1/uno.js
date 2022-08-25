8ec2a1754f622f04cf4ecf8435f0ac4783b4af76
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-25 16:33:00 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -2,7 +2,7 @@ let frase = prompt("Ingrese la frase a mostrar en pantalla...");
 
 console.log(frase)
 
-if frase == null:
-     document.write("<h1>¡No pusiste nada!</h1>");
-else:
-     document.write("<h1>" + frase + "</h1>");
+//
+//     document.write("<h1>¡No pusiste nada!</h1>");
+//else:
+//     document.write("<h1>" + frase + "</h1>");

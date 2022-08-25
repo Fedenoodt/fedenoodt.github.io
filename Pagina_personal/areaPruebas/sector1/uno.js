880dee797534e77d53a7b4cfb2155fd50a1f61db
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-25 17:31:16 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -2,4 +2,4 @@ let frase = prompt("Ingrese la frase a mostrar en pantalla...");
 
 console.log(frase)
 
-document.write("<h1>Lo que escribiste: " + frase + "</h1>");
+document.write("<h1>Lo que escribiste: " + %s frase + "</h1>");

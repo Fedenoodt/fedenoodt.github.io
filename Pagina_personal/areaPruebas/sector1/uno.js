8d4661036502ac39b0975f140ef45d02d3292af9
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-26 11:26:24 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -1,6 +1,4 @@
 Let humane = window.confirm("¿Es usted HuMaN0?")
 
-if humane:
-    document.write("¡Vamo' todav&iacute;a!")
-else:
-    document.write("INTRUSO. INTRUSO. INTRUSO.")
+if (humane) {document.write("¡Vamo' todav&iacute;a!")}
+else {document.write("INTRUSO. INTRUSO. INTRUSO.")}

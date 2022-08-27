93e47264a78d20573c8b8328455b4614ee9e3989
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-27 17:10:30 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -1,1 +1,7 @@
-apendChild("<h1> HOLA </h1>")
+const contenedor = document.querySelector(.contenedor);
+const parafraseador = document.createElement("P");
+let textoIngresar = prompt("Ingrese un texto...")
+const textoIngresado = document.createTextNode(textoIngresar)
+
+parafraseador.apendChild(textoingresado)
+contenedor.apendChild(parafraseador)

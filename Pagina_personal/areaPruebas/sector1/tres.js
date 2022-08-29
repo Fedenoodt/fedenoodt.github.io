8ae9584f4682d3433ed69224db539503019fcf2e
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-29 11:33:18 -0300

    Update tres.js

--- Pagina_personal/areaPruebas/sector1/tres.js
+++ Pagina_personal/areaPruebas/sector1/tres.js
@@ -4,6 +4,6 @@ numero3 = 2;
 numero4 = 14;
 numero5 = 7;
 
-vl = (numero4 / numero3 == numero7 && numero4 / numero5 == numero3) && (numero4 - numero5 == numero5) || (numero1 <= numero2) && ((numero1 - numero4) < numero4);
+vl = (numero4 / numero3 == numero5 && numero4 / numero5 == numero3) && (numero4 - numero5 == numero5) || (numero1 <= numero2) && ((numero1 - numero4) < numero4);
 
 document.write(vl);

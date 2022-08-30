6d62b0a496c4dcf4fedfe85d9304df82587db0f4
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-08-30 20:54:13 -0300

    Update uno.js

---
+++ Pagina_personal/areaPruebas/sector2/uno.js
@@ -0,0 +1,10 @@
+let numero = 460;
+let adivino = prompt("Adivine el numero minimo...");
+
+if (adivino => numero) {
+alert("Usted ha adivinado.")
+};
+
+else if (adivino < numero) {
+alert("No. El numero es mas grande")
+};

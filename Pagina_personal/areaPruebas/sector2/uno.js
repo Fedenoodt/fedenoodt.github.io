5ec40281710b5b110acb46d1a97346c9109db7cb
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-09-01 16:29:00 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector2/uno.js
+++ Pagina_personal/areaPruebas/sector2/uno.js
@@ -1,10 +1,12 @@
-let numero = 460;
-let adivino = prompt("Adivine el numero minimo...");
+let aleatorio = Math.random() * 499;
+let numero = Math.round(aleatorio)
+let adivino = prompt("Adivine el numero minimo, de entre una y tres cifras...");
 
-if (adivino => numero) {
-alert("Usted ha adivinado.")
-};
+if (adivino >= numero) {
+alert("Usted ha adivinado.");
+}
 
 else if (adivino < numero) {
-alert("No. El numero es mas grande")
+alert("No. El numero es mas grande");
 };
+document.write('<h1>El n&uacute;mero m&iacute;nimo es ' + numero + ".</h1><br /><br /><h1>(El m&aacute;ximo n&uacute;mero aleatorio es 500)</h1>")

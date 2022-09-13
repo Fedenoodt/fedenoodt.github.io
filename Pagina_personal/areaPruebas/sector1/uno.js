fc4cf62c4bda76b36db7d7ed434eefc9fb28bb57
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-09-13 10:29:47 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -4,9 +4,4 @@ hacerCosas = function() {
     let valorB = "Homo Sapiens Sapiens";
   document.write(valorA, valorB);
 }
-touch = false;
-document.write(touch);
-document.write(`<a class = "ejecutor" href="touch = true;"><h1>BOTON</h1></a>`);
-seleccionador = document.querySelector("a");
-document.write(seleccionador);
-document.write(touch);
+document.write(`<input type = "number" name =""></input>`);

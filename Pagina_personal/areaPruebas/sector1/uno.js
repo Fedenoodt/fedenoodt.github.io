940689a49d07a82a7744f410cf0ef8bbe76f8c1e
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-09-27 19:58:16 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -1,11 +1,1 @@
-
-hacerCosas = function() {
-    let valorA = 85;
-    let valorB = "Homo Sapiens Sapiens";
-  document.write(valorA, valorB);
-}
-
-<input type = "number" name =""></input>
-<label>Número del producto</label>
-<input type = "button" name =""></input>
-<label>Agregar al carrito</label>
+let yo = localStorage.setItem("nombre", "Fedenoodt")

9f86f744a94c620ab7e443da01bd7e769671af88
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-09-28 11:57:49 -0300

    Update dos.js

--- Pagina_personal/areaPruebas/sector1/dos.js
+++ Pagina_personal/areaPruebas/sector1/dos.js
@@ -1,6 +1,11 @@
-function nuevoContenido() {
-alert("carga el contenido nuevo");
-document.open();
-document.write("<h1>Quita el contenido viejo - Agrega el contenido nuevo!</h1>");
-document.close();
+const productos = [{ id: 1, nombre: "Arroz", precio: 125 },
+                   { id: 2, nombre: "Nesquik", precio: 433 },
+                   { id: 3, nombre: "Zanahoria dorada", precio: 134309 }];
+
+for (const producto of productos) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `<h3> ID: ${producto.id}</h3>;
+                            <p> Producto: ${producto.nombre}</p>;
+                            <b> $ ${producto.precio}</b>;`;
+    document.body.appendChild(contenedor);
 }

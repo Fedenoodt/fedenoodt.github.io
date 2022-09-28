c045c9cb99ae45894a40d45c07682282dc8d4fff
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-09-28 16:38:39 -0300

    Update dos.js

--- Pagina_personal/areaPruebas/sector1/dos.js
+++ Pagina_personal/areaPruebas/sector1/dos.js
@@ -55,14 +55,15 @@ const productos = [{ id: 1, nombre: "perrosLittle", precio: 125 },
 
 function ordenador () {
     const contenedor = document.createElement("div");
+    contenedor.innerHTML = `<div>${
     for (const producto of productos) {
         let articulo = document.createElement("div");
         articulo.innerHTML = `<h3> ID: ${producto.id}</h3>
                                 <p> Producto: ${producto.nombre}</p>
                                 <b> $ ${producto.precio}</b>`;
-        contenedor.innerHTML = `<div>${articulo()}</div>`;
-        document.body.appendChild(contenedor);
     }
+    }</div>`;
+        document.body.appendChild(contenedor);
 }
 
 ordenador()

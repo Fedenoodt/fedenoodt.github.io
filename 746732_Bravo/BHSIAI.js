b79fb92537f98ef552d2d0052d97957f6fc1dbcd
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-08 10:36:05 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -1,5 +1,7 @@
 console.log(`Estructura Javascript conectada con éxito.-`);
 
+// ======================================================================================================================================= //
+
 let cabeza = `
 <!DOCTYPE html>
 <html>
@@ -51,49 +53,72 @@ let cabezal = `
     <hr />
     <hr />`;
 
-let central = document.createElement("div");
-central.innerHTML = `
-${cabezal}
-<div class="in-flex">
-<h2>Accesos de emergencia</h2>
-<ul>
-    <li><a href="https://github.com/Fedenoodt/CH---JavaScript/edit/main/Notas.txt" target="_blank">¡Write now! - CoderHouse</a></li>
-    <li><a href="https://coderhouse.zoom.us/j/98016948428?pwd=cW9HdVBHZy9HOTRsTll4UkNBSkZrQT09&prefer=1&un=RmVkZXJpY28gTm9vZHQgTW9saW5z#success" target="_blank">Clases - CoderHouse</a></li>
-    <li><a href="https://github.com/Fedenoodt/JavaScript/edit/main/Notas.txt#new_blob.js-blob-form" target="_blank">¡Write now! - JavaScript</a></li>
-    <li><a href="https://github.com/Fedenoodt/CH---JavaScript" target="_blank">GitHub - JavaScript - CoderHouse</a></li>
-    <li><a href="https://github.com/Fedenoodt/JavaScript/tree/main" target="_blank">GitHub - JavaScript</a></li>
-    <li><a href="https://fedenoodt.github.io/Pagina_personal/javascript-es.7.pdf" target="_blank">Manual-Informatica.com - JavaScript</a></li>
-    <li><a href="https://www.youtube.com/watch?v=z95mZVUcJ-E&t=2756s" target="_blank">Soy Dalto - JavaScript</a></li>
-</ul>
-<hr />		
-<ul>
-    <li class="listaHorizontal"><a href="https://mail.google.com/mail/u/1/#inbox" target="_blank">Gmail</a></li>
-    <li class="listaHorizontal"><a href="https://github.com/" target="_blank">GitHub</a></li>
-    <li class="listaHorizontal"><a href="https://github.com/Fedenoodt/Trabajo" target="_blank">Trabajo</a></li>
-    <li class="listaHorizontal"><a href="https://drive.google.com/drive/u/1/my-drive" target="_blank">Drive</a></li>
-    <li class="listaHorizontal"><a href="https://app.diagrams.net/" target="_blank">Diagramas</a></li>
-    <li class="listaHorizontal"><a href="https://classroom.google.com/u/1/h" target="_blank">Classroom</a></li>
-    <li class="listaHorizontal"><a href="https://www.youtube.com/" target="_blank">Youtube</a></li>
-    <li class="listaHorizontal"><a href="https://calendar.google.com/calendar/u/1/r?tab=oc" target="_blank">Calendario</a></li>
-    <li class="listaHorizontal"><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIHerramientas.html">M&aacutes herramientas...</a></li>
-</ul>
-<hr />
-<h2>Links de acceso</h2>
-<ul>
-    <li><a href="https://plataforma.coderhouse.com/cursos" target="_blank">Coder House - Plataforma E-Learning</a>></li>
-    <li><a id = "webs" href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIWebs.html">P&aacute;ginas Especiales</a>></li>
-    <li><a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfGshu5XTZ23OWK8cRft5F8m" target="_blank">Jedi Order Playlist</a>></li>
-    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIMediosAudiovisuales.html">Medios audiovisuales</a>></li>
-    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIComunicaciones.html">Comunicaciones</a>></li>
-    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIRedesSociales.html">Redes Sociales</a>></li>
-</ul>
-<br />
+function base () {
+	let cuerpo = document.createElement("div");
+	cuerpo.innerHTML = `
+	${cabezal}
+	<div class="in-flex">
+		<h2>Accesos de emergencia</h2>
+		<ul>
+		    <li><a href="https://github.com/Fedenoodt/CH---JavaScript/edit/main/Notas.txt" target="_blank">¡Write now! - CoderHouse</a></li>
+		    <li><a href="https://coderhouse.zoom.us/j/98016948428?pwd=cW9HdVBHZy9HOTRsTll4UkNBSkZrQT09&prefer=1&un=RmVkZXJpY28gTm9vZHQgTW9saW5z#success" target="_blank">Clases - CoderHouse</a></li>
+		    <li><a href="https://github.com/Fedenoodt/JavaScript/edit/main/Notas.txt#new_blob.js-blob-form" target="_blank">¡Write now! - JavaScript</a></li>
+		    <li><a href="https://github.com/Fedenoodt/CH---JavaScript" target="_blank">GitHub - JavaScript - CoderHouse</a></li>
+		    <li><a href="https://github.com/Fedenoodt/JavaScript/tree/main" target="_blank">GitHub - JavaScript</a></li>
+		    <li><a href="https://fedenoodt.github.io/Pagina_personal/javascript-es.7.pdf" target="_blank">Manual-Informatica.com - JavaScript</a></li>
+		    <li><a href="https://www.youtube.com/watch?v=z95mZVUcJ-E&t=2756s" target="_blank">Soy Dalto - JavaScript</a></li>
+		</ul>
+		<hr />		
+		<ul>
+		    <li class="listaHorizontal"><a href="https://mail.google.com/mail/u/1/#inbox" target="_blank">Gmail</a></li>
+		    <li class="listaHorizontal"><a href="https://github.com/" target="_blank">GitHub</a></li>
+		    <li class="listaHorizontal"><a href="https://github.com/Fedenoodt/Trabajo" target="_blank">Trabajo</a></li>
+		    <li class="listaHorizontal"><a href="https://drive.google.com/drive/u/1/my-drive" target="_blank">Drive</a></li>
+		    <li class="listaHorizontal"><a href="https://app.diagrams.net/" target="_blank">Diagramas</a></li>
+		    <li class="listaHorizontal"><a href="https://classroom.google.com/u/1/h" target="_blank">Classroom</a></li>
+		    <li class="listaHorizontal"><a href="https://www.youtube.com/" target="_blank">Youtube</a></li>
+		    <li class="listaHorizontal"><a href="https://calendar.google.com/calendar/u/1/r?tab=oc" target="_blank">Calendario</a></li>
+		    <li class="listaHorizontal"><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIHerramientas.html">M&aacutes herramientas...</a></li>
+		</ul>
+		<hr />
+		<h2>Links de acceso</h2>
+		<ul>
+		    <li><a href="https://plataforma.coderhouse.com/cursos" target="_blank">Coder House - Plataforma E-Learning</a>></li>
+		    <li><a id = "webs" href=${webs()}>P&aacute;ginas Especiales</a>></li>
+		    <li><a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfGshu5XTZ23OWK8cRft5F8m" target="_blank">Jedi Order Playlist</a>></li>
+		    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIMediosAudiovisuales.html">Medios audiovisuales</a>></li>
+		    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIComunicaciones.html">Comunicaciones</a>></li>
+		    <li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIRedesSociales.html">Redes Sociales</a>></li>
+		</ul>
+		<br />
 
-</div>
-</section>
-`;
-document.body.appendChild(central);
+	    </div>
+	</section>
+	`;
+	document.body.appendChild(cuerpo);
+}
+
+base()
+
+function webs () {
+    let cuerpo = document.createElement("div");
+    cuerpo.innerHTML = `
+    ${cabezal}
+    <div class="in-flex">
+			<h2>P&aacute;ginas Especiales</h2>
+			<ul>
+				<li><a href="https://fedenoodt.github.io/Pagina_personal/vida/inicio.html" target="_blank">Pagina Oficial de Fedenoodt</a>></li>
+				<li><a href="https://fedenoodt.github.io/Pagina_personal/vida/nexoGrupote.html" target="_blank">Nexo Grupote</a>></li>
+			 	<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAI.html#top">Volver</a><<</li>
+			</ul>
+		</div>
 
 
+	</section>
+    `;
+    document.body.appendChild(cuerpo);
+}
+
+// ======================================================================================================================================= //
 
 console.log(`Ejecución de Javascript finalizada.-`)

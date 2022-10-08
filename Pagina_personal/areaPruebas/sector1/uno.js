8ea0f5422da043548f2d9613117a17c842e798b2
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-08 14:18:57 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -1,3 +1,4 @@
 document.write(`<h1 id = "txt">Hola...</h1>`);
 let entrada = document.getElementById("txt");
-entrada.keydown = () => (document.write("¡Pvto el que lee!"));  
+console.log("entrada");
+entrada.keydown = () => (console.log("¡Pvto el que lee!"));  

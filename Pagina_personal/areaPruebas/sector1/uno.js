b37774083b7346f9a686430c30b77b50c946fb00
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-14 16:17:47 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -4,9 +4,15 @@ function estableceHoras() {
     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     let mes = meses[dt.month - 1];
     const hora = `${dt.hour}:${dt.minute}:${dt.second}hs. ${dt.day} de ${mes} de ${dt.year}`;
-    return hora
+    
 }
 
 //setInterval(document.write(estableceHoras, 1000)
 
-estableceHoras()
+//estableceHoras()
+
+const DateTime = luxon.DateTime
+    const dt = DateTime.now();
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    let mes = meses[dt.month - 1];
+    const hora = `${dt.hour}:${dt.minute}:${dt.second}hs. ${dt.day} de ${mes} de ${dt.year}`;

60da1712051adb211f6aa7040672dd64af1829f9
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-14 14:09:44 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -3,7 +3,7 @@ function giraHoras() {
     const dt = DateTime.now();
     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
     let mes = meses[dt.month - 1];
-    const hora = `${dt.hour}:${dt.minute}:${dt.seconds}hs. ${dt.day} de ${mes} de ${dt.year}`;
+    const hora = `${dt.hour}:${dt.minute}:${dt.second}hs. ${dt.day} de ${mes} de ${dt.year}`;
     return hora
 }
 

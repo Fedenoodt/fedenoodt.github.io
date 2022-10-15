8601d0913dca381225c8f0677688b3efb36291c8
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-15 11:32:39 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -17,10 +17,10 @@ const DateTime = luxon.DateTime
 const dt = DateTime.now();
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
 let restado = meses[dt.month - 1];
-let mes = ceros(restado);
-let dia = ceros(dt.day);
-let hora = ceros(dt.hour)
-let minuto = ceros(dt.minute)
-let segundos = ceros(dt.second)
-const actual = `${segundos}:${minuto}:${hora}hs. ${} de ${mes} de ${dt.year}`;
+let mes = restado;
+let dia = dt.day;
+let hora = dt.hour;
+let minuto = dt.minute;
+let segundos = dt.second;
+const actual = `${ceros(hora)}:${ceros(minuto)}:${ceros(segundos)}hs. ${ceros(dia)} de ${ceros(mes)} de ${dt.year}`;
 setInterval(document.write(actual), 1000)

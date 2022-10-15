61cea3fda07ab727ca6bde2f916e7f60e5d0935e
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-15 19:54:43 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -9,18 +9,22 @@ function estableceHoras () {
 
 const DateTime = luxon.DateTime
 const dt = DateTime.now();
+const anio = dt.year;
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-let mes = '';
+let NUMes  = '';
+let mes = meses[dt.month - 1];
 let dia = '';
 let hora = '';
 let minuto = '';
 let segundos = '';
 
-(dt.month - 1) < 10 ? mes = '0' + meses[dt.month - 1] : mes = meses[dt.month - 1]
+(dt.month - 1) < 10 ? NUMes = '0' + dt.month - 1 : NUMes = dt.month - 1
 dt.day < 10 ? dia = '0' + dt.day : dia = dt.day
 dt.hour < 10 ? hora = '0' + dt.hour : hora = dt.hour
 dt.minute < 10 ? minuto = '0' + dt.minute : minuto = dt.minute
 dt.second < 10 ? segundos = '0' + dt.second : segundos = dt.second
 
-const actual = `${hora}:${minuto}:${segundos}hs. ${dia} de ${mes} de ${dt.year}`;
-setInterval(document.write(actual), 1000)
+const fecha = `${hora}:${minuto}:${segundos}hs. ${dia} de ${NUMes} de ${anio}`;
+const tactica = `${hora}${minuto}:${segundos}  ${dia}${NUMes}${anio}`;
+document.write(fecha)
+document.write(tactica)

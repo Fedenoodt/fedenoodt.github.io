cd4584cdd8a11716d4dc87d3dd8c879077fdf864
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-10-15 10:18:29 -0300

    Update uno.js

--- Pagina_personal/areaPruebas/sector1/uno.js
+++ Pagina_personal/areaPruebas/sector1/uno.js
@@ -1,4 +1,4 @@
-function estableceHoras() {
+function estableceHoras () {
     const DateTime = luxon.DateTime
     const dt = DateTime.now();
     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
@@ -7,12 +7,21 @@ function estableceHoras() {
     
 }
 
-
+function ceros (valor) {
+    if valor < 10:
+        valor = '0' + valor;
+    return valor
+}
 
 
 const DateTime = luxon.DateTime
 const dt = DateTime.now();
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-let mes = meses[dt.month - 1];
-const hora = `${dt.hour}:${dt.minute}:${dt.second}hs. ${dt.day} de ${mes} de ${dt.year}`;
-setInterval(document.write(hora), 1000)
+let restado = meses[dt.month - 1];
+let mes = ceros(restado);
+let dia = ceros(dt.day);
+let hora = ceros(dt.hour)
+let minuto = ceros(dt.minute)
+let segundos = ceros(dt.second)
+const actual = `${segundos}:${minuto}:${hora}hs. ${} de ${mes} de ${dt.year}`;
+setInterval(document.write(actual), 1000)

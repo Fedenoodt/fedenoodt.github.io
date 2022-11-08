13bd7f9ad79742e7bb87c4f499306a1d3bbf0a3f
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-11-08 17:30:51 -0300

    Update nodoJS.js

--- 746732_Bravo/nodoJS.js
+++ 746732_Bravo/nodoJS.js
@@ -1,7 +1,20 @@
+
+
+// 'x' y 'z' vienen a representar a cada una de las imagenes... 
+function x () {
+    let saludar = 'Hola';
+    return saludar
+}
+
+function z () {
+    let interrogante = '¿Como estas? Espero que muy bien.'
+    return interrogante
+}
+
 let saludar = 'Hola';
 let interrogante = '¿Como estas? Espero que muy bien.'
-let cabezal = `<p onclick = pendiente(saludar)>¡Saludo!</p>
-<p onclick = pendiente(interrogante)>¿Como estas?</p>`;
+let cabezal = `<p onclick = pendiente(x())>¡Saludo!</p>
+<p onclick = pendiente(z())>¿Como estas?</p>`;
 
 a = document.createElement('div');
 a.innerHTML = `<br /><br /><p>${localStorage.getItem('valor')}</p>`;
@@ -14,5 +27,13 @@ function pendiente (valor) {
     localStorage.clear()
     const enJSON = JSON.stringify(valor);
     localStorage.setItem('valor', enJSON)
-    return valor
+    cuerpo()
+}
+function cuerpo () {
+    // La idea es levantar desde acá, toda la ejecución, ya que es más nítido de comparar con el localStorage().
+    // Ahi armamos una "funcion de lectura" que comprenda que imagen del nexo (contemporánea, ya que no esta 
+    // listo este nodo) tiene que emprender.
+    let pasaje = localStorage.getItem('valor');
+
+
 }

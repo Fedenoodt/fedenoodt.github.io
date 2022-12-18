4a53871218015781d2db7e51caa5faaee402f86d
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-12-18 00:22:06 -0300

    Update qatar.js

--- Pagina_personal/ocio/especialQatar/qatar.js
+++ Pagina_personal/ocio/especialQatar/qatar.js
@@ -1,40 +1,4 @@
-// function relojCONF () {
-//     // location.reload(4000)
-//     const DateTime = luxon.DateTime
-//     const dt = DateTime.now();
-//     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-//     let mes = meses[dt.month - 1];
-
-//     let dia = 24;
-//     let marcaH = 12;
-//     let marcaM = 0;
-
-//     let horas = dt.hour;
-//     if (horas < 10) {
-//         horas = '0' + dt.hour;
-//     }
-//     let minutos = dt.minute;
-//     if (minutos < 10) {
-//         minutos = '0' + dt.minute;
-//     }
-    
-//     const hora = `${horas}:${minutos}hs. ${dt.day} de ${mes} de ${dt.year}`;
-
-//     return hora
-// }
-// // let getReloj = relojCONF();
-// const timeDateInterval = setInterval(() => {
-//     const time = relojCONF();
-//     localStorage.setItem('reloj', time.toString());
-// }, 1000);
-
-// function darReloj () {
-// 	let pantalla = document.getElementById('reloj');
-// 	let reloj = localStorage.getItem('reloj');
-// 	pantalla.innerHTML = reloj;
-// }
-
-// setInterval(darReloj, 1)
+localStorage.clear()
 
 function relojCONF () {
     // location.reload(4000)
@@ -53,16 +17,19 @@ function relojCONF () {
 
     if (horas > marcaH) {
         horas = (dia - horas) + marcaH - 1;
-    } else {
-        horas = marcaH - horas;
+    } else if (horas < marcaH) {
+        horas = marcaH - (horas + 1);
     }
-    if (horas < 10) {
-        horas = '0' + dt.hour;
-    }
+    // if (horas < 10) {
+    //     horas = '0' + dt.hour;
+    // }
 
-    if (minutos < 10) {
-        minutos = '0' + dt.minute;
-    }
+    // if (minutos < 10) {
+    //     minutos = '0' + dt.minute;
+    // }
+
+    localStorage.setItem('actualH', dt.hour)
+    localStorage.setItem('actualM', dt.minute)
 
     // let horas = 0;
     // let minutos = 0;

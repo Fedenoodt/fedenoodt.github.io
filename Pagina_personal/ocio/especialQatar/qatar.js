32304694054c955382c5af83318371c0c84b4a27
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-12-17 22:34:06 -0300

    Add files via upload

---
+++ Pagina_personal/ocio/especialQatar/qatar.js
@@ -0,0 +1,83 @@
+// function relojCONF () {
+//     // location.reload(4000)
+//     const DateTime = luxon.DateTime
+//     const dt = DateTime.now();
+//     const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+//     let mes = meses[dt.month - 1];
+
+//     let dia = 24;
+//     let marcaH = 12;
+//     let marcaM = 0;
+
+//     let horas = dt.hour;
+//     if (horas < 10) {
+//         horas = '0' + dt.hour;
+//     }
+//     let minutos = dt.minute;
+//     if (minutos < 10) {
+//         minutos = '0' + dt.minute;
+//     }
+    
+//     const hora = `${horas}:${minutos}hs. ${dt.day} de ${mes} de ${dt.year}`;
+
+//     return hora
+// }
+// // let getReloj = relojCONF();
+// const timeDateInterval = setInterval(() => {
+//     const time = relojCONF();
+//     localStorage.setItem('reloj', time.toString());
+// }, 1000);
+
+// function darReloj () {
+// 	let pantalla = document.getElementById('reloj');
+// 	let reloj = localStorage.getItem('reloj');
+// 	pantalla.innerHTML = reloj;
+// }
+
+// setInterval(darReloj, 1)
+
+function relojCONF () {
+    // location.reload(4000)
+    const DateTime = luxon.DateTime
+    const dt = DateTime.now();
+    const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+    let mes = meses[dt.month - 1];
+
+    let dia = 24;
+    let hora = 60;
+    let marcaH = 12;
+    let marcaM = 0;
+
+    let horas = dt.hour;
+    let minutos = hora - dt.minute;
+
+    if (horas > marcaH) {
+        horas = (dia - horas) + marcaH;
+    } else {
+        horas = marcaH - horas;
+    }
+    if (horas < 10) {
+        horas = '0' + dt.hour;
+    }
+
+    if (minutos < 10) {
+        minutos = '0' + dt.minute;
+    }
+ 
+    const pantalla = `${horas} horas, ${minutos} minutos para el inicio de juego`;
+
+    return pantalla
+}
+// let getReloj = relojCONF();
+const timeDateInterval = setInterval(() => {
+    const time = relojCONF();
+    localStorage.setItem('reloj', time.toString());
+}, 1000);
+
+function darReloj () {
+	let pantalla = document.getElementById('reloj');
+	let reloj = localStorage.getItem('reloj');
+	pantalla.innerHTML = reloj;
+}
+
+setInterval(darReloj, 1)

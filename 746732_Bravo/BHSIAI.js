817cf89c0ba9b27c71601a3a31a976c3eba24bae
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2022-12-27 08:39:33 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -287,8 +287,30 @@ function radio () {
 				<li><a href="https://bluefm.com.ar/" target="_blank">Radio Blue 100.7</a>></li>
 				<li><a href="https://fmaspen.com/category/aspen-time/" target="_blank">Radio Aspen 102.3</a>></li>
 				<li><a href="https://www.radiozoe.com.ar/" target="_blank">Radio Zoe 107.1</a>></li>
+				<hr />
 				<li><a href="https://www.youtube.com/watch?v=ceqgwo7U28Y&ab_channel=thebootlegboy2" target="_blank">the bootleg boy 2 ~ Chill Radio 24/7 Youtube</a>></li>
 				<li><a href="https://www.youtube.com/watch?v=nSy7wpevIsY&list=PLOJvby8Q_tfG83AsSJSbxPv7-C9VUvRha&ab_channel=hiphopmusic" target="_blank">Radio Los Santos Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=wsd2xgm91nM&ab_channel=GTAFM" target="_blank">GTAV ♣ STILL SLIPPING Los Santos • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=d0hKoOw0Wuw&ab_channel=GTAFM" target="_blank">GTAV ♣ iFruit Radio • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=YYIBtojKDSc&ab_channel=GTAFM" target="_blank">GTAV ♣ Los Santos Rock Radio • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=uZ3RFNMKcmg&ab_channel=GTAFM" target="_blank">GTAV ♣ Non Stop Pop FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=nxwUWlqdCFI&ab_channel=SilverAsh" target="_blank">GTAV ♣ Radio Los Santos • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=tmQqUh0xark&ab_channel=ListenToThis" target="_blank">GTAV ♣ Channel X • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=N12WWl_f3QM&ab_channel=ListenToThis" target="_blank">GTAV ♣ Rebel Radio • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=E-PFWBfRwAc&ab_channel=semmelsamu" target="_blank">GTAV ♣ Soulwax FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=iX2liYJZuJU&ab_channel=xcgac" target="_blank">GTAV ♣ East Los FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=iDFQd0AGmLs&ab_channel=GTAFM" target="_blank">GTAV ♣ West Coast Classics Radio Station • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=aaXui87cF5Y&ab_channel=GTAFM" target="_blank">GTAV ♣ Blaine County Talk Radio • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=osmrXqRuwJA&ab_channel=ListenToThis" target="_blank">GTAV ♣ The Blue Ark • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=fYi-ZoglszY&t=4308s&ab_channel=GTAFM" target="_blank">GTAV ♣ WorldWide FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=kEkNJRQJACI&ab_channel=videodrom83" target="_blank">GTAV ♣ FlyLo FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=kEkNJRQJACI&ab_channel=videodrom83" target="_blank">GTAV ♣ The Lowdown 91.1 • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=R9nFhKZhTJ0&ab_channel=PeterDazuk" target="_blank">GTAV ♣ The Lab Radio Station • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=AA3WjWD_hyM&ab_channel=ListenToThis" target="_blank">GTAV ♣ Radio Mirror Park • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=lCZc9y9KpY4&ab_channel=ListenToThis" target="_blank">GTAV ♣ Space 103.2 • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=y-Faj25nOhM&ab_channel=ListenToThis" target="_blank">GTAV ♣ Vinewood Boulevard Radio • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=-W6g6QUhAj0&ab_channel=Tate%27sDailySpeech" target="_blank">GTAV ♣ Blonded Los Santos 97 8 FM • Youtube</a>></li>
+				<li><a href="https://www.youtube.com/watch?v=m8HlukW1auQ&ab_channel=videodrom83" target="_blank">GTAV ♣ LSUR • Youtube</a>></li>
 				<li></a><a href = "" onclick = pendiente(conversor(${mediosID}))>Volver</a><<</li>
 			</ul>
 			<a class = "inicio" href = "" onclick = pendiente(conversor(${baseID}))><h2><<<<< Volver al inicio <<<</h2></a>

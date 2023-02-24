cc367ec0a3002a3f1720f8cb1589646f6cc21fd3
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-02-24 13:56:12 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -186,14 +186,14 @@ function base () {
 
 function driving () {
     const mesActual = localStorage.getItem('mes');
-    //if (mesActual == 1 | mesActual == 2) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfGp8QXNce2UAKUuS5DBjSbR" target="_blank">Tour de paseos</a>`; }
-    //else if (mesActual == 3 | mesActual == 4) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEQRUIpoJ7dSyIKed8_abKe" target="_blank">Tour de paseos</a>`; }
-    //else if (mesActual == 5 | mesActual == 6) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEfzeRD7jhLZ4cVOv9NaJHd" target="_blank">Tour de paseos</a>`; }
-    //else if (mesActual == 7 | mesActual == 8) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEvtLSuftj_-HGbm3s1sf9v" target="_blank">Tour de paseos</a>`; }
-    //else if (mesActual == 9 | mesActual == 10) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfHnP5MiUWhW-6cCS4iu8iZX" target="_blank">Tour de paseos</a>`; }
-    //else if (mesActual == 11 | mesActual == 12) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfFi63ckq8rJXQn3uAKGPiWK" target="_blank">Tour de paseos</a>`; }
+    if (mesActual == 1 | mesActual == 2) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfGp8QXNce2UAKUuS5DBjSbR" target="_blank">Tour de paseos</a>`; }
+    else if (mesActual == 3 | mesActual == 4) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEQRUIpoJ7dSyIKed8_abKe" target="_blank">Tour de paseos</a>`; }
+    else if (mesActual == 5 | mesActual == 6) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEfzeRD7jhLZ4cVOv9NaJHd" target="_blank">Tour de paseos</a>`; }
+    else if (mesActual == 7 | mesActual == 8) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfEvtLSuftj_-HGbm3s1sf9v" target="_blank">Tour de paseos</a>`; }
+    else if (mesActual == 9 | mesActual == 10) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfHnP5MiUWhW-6cCS4iu8iZX" target="_blank">Tour de paseos</a>`; }
+    else if (mesActual == 11 | mesActual == 12) { let lista = `<a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfFi63ckq8rJXQn3uAKGPiWK" target="_blank">Tour de paseos</a>`; }
     let link = document.getElementById('driving');
-    if (mesActual == 2) {link.innerHTML = "SIIIII"}
+    link.innerHTML(lista)
 }
 driving
 

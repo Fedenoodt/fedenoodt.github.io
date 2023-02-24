b34294efb32da4c03aa89d2873fe72982f3a5add
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-02-24 05:22:01 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -197,6 +197,7 @@ function driving () {
 }
 
 const lista = driving;
+console.log(lista)
 
 function tools () {
     let cuerpo = document.createElement("div");
@@ -227,7 +228,7 @@ function tools () {
 		<li><a href="https://www.speedtest.net/es" target="_blank">Speedtest Ookla</a>></li>
 		<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAIPruebas.html">Zona de pruebas web</a>></li>
 		<li><a href="https://c.tenor.com/2wR9-9cmrjMAAAAC/ahora-vengo-hernan-drago.gif" target="_blank">Ahora vengo</a>></li>
-		<li><a href="${lista}" target="_blank">Tour de paseos</a>></li>
+		<!--<li><a href="${lista}" target="_blank">Tour de paseos</a>></li>-->
 		<li><a href="https://www.youtube.com/playlist?list=PLOJvby8Q_tfGmVbV4xxkfLjoQF6FjybsL" target="_blank">Colección de salvapantallas</a>></li>
 		<li><a href="https://ayuda.ch-sistemas.com/wp-content/uploads/2018/05/cabina4.png" target="_blank">Fondo de telón</a>></li>
 		<li><a href="https://i.pinimg.com/originals/ae/a0/9d/aea09d635db4444475efac0b42e0ac1b.gif" target="_blank">Bandera Argentina</a>></li>

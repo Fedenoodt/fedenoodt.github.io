527da215445d146607133e25a0e5fb291b665c96
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-03-16 12:33:10 -0300

    Create shelvMixTools.js

---
+++ shelvMixTools.js
@@ -0,0 +1,61 @@
+const tecTools = 'Shelving informa:'
+
+function JStringify (parameter) {
+    return JSON.stringify(parameter)
+}
+
+function JSparse (parameter) {
+    return JSON.parse(parameter)
+}
+
+
+function consoleValidator (parameter) {
+    let valid = false;
+    if (parameter === null || parameter == null || parameter == 'null') {
+        consoleWarn(tecTools, `ERROR DE FORMATO: VARIABLE VACIA.`)
+    }
+    else if (parameter === undefined || parameter == undefined || parameter == 'undefined') {
+        consoleWarn(tecTools, `ERROR DE FORMATO: VARIABLE INDEFINIDA.`)
+    }
+    else {
+        valid = true;
+    }
+    return valid
+}
+
+function consoleLog (parameter) {
+    console.log(parameter)
+}
+function consoleInfo (parameter) {
+    console.info(parameter)
+}
+function consoleWarn (parameter) {
+    console.warn(parameter)
+}
+function consoleError (parameter) {
+    console.error(parameter)
+}
+
+function consoleLogSecure (parameter) {
+    if (consoleValidator(parameter)) {
+        consoleInfo(`${tecTools} Parametro evaluado, y validado exitosamente.`)
+        consoleLog(parameter)
+    }
+}
+
+function nullUndefined (parameter) {
+    let charged = false;
+    if (parameter === null || parameter === undefined || parameter == null || parameter == undefined || parameter == 'null' || parameter == 'undefined') {
+        charged = true;
+        consoleLog(`${tecTools} La variable es vacia o indefinida.`)
+    }
+    return charged
+}
+
+function localSet (key, value) {
+    localStorage.setItem(key, value)
+}
+
+function localGet (key) {
+    return localStorage.getItem(key)
+}

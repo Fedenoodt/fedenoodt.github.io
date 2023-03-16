10be1f13f565d4d783c4d36317f774af888cf981
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-03-16 19:12:35 -0300

    Update odtu.js

--- Pagina_personal/ocio/odtu.js
+++ Pagina_personal/ocio/odtu.js
@@ -33,10 +33,10 @@ const index = `
                     <center>
                         <img class="slFlag" src="https://fedenoodt.github.io/Pagina_personal/Organizacion_dispar_tierras_Unidas.png"><a href = "https://github.com/Fedenoodt/fedenoodt.github.io/tree/gh-pages/Pagina_personal/ocio" target="_blank"><img class="imgAtajos" src="https://fedenoodt.github.io/Pagina_personal/768px-Settings_(iOS).png"></a><h1>Organizaci&oacute;n dispar de tierras unidas</h1>
                         <h2>Direcci&oacute;n IP:</h2>
-                        <h3 id = 'IP'>${ip}</h3><br /><br />
+                        ${refresh(ip)}<br /><br />
                         <br /><hr /><br />
 
-                        <div id = 'lobby'>${status}</div>
+                        <div id = 'lobby'>${refresh(status)}</div>
                         <br /><hr /><br />
                         <br /><hr /><br />
                         <ul>
@@ -178,7 +178,7 @@ const hamachi = `
                     <h1>Dirección Hamachi</ h1>
                     <h3>ODTU1191_01</h3>
                     <h3>*bostadecaballo*</h3></br></br>
-                    <div id = 'server'>${statusH}</div>
+                    <div id = 'server'>${refresh(statusH)}</div>
 
                     </br></hr></br>
                     <h3>Link de descarga: <a href="https://www.vpn.net/">https://www.vpn.net/</a></h3>

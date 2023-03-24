97fd4012c7ab31f208b495aa155debce9262ac3a
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-03-23 22:25:14 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -484,7 +484,7 @@ function minecraft () {
 				<li><a href="https://docs.google.com/document/d/1b4P51y2acdchT11xjx-eywD-BodEp82eL3X0F1yKB3I/edit" target="_blank">Salvamundos</a>></li>
 				<hr />
 				<li><a href="https://fedenoodt.github.io/Pagina_personal/ocio/La_Fortaleza/IPLaFortaleza.html" target="_blank">La Fortaleza</a>></li>
-				<li><a href="https://fedenoodt.github.io/Pagina_personal/ocio/IP[NUEVO_MUNDO].html" target="_blank">Organización Dispar de Tierras Unidas</a>></li>
+				<li><a href="https://fedenoodt.github.io/Pagina_personal/ocio/odtu.html" target="_blank">Organización Dispar de Tierras Unidas</a>></li>
 				<li></a><a href = "" onclick = pendiente(conversor(${juegosID}))>Volver</a><<</li>
 		</ul>
 		<a class = "inicio" href = "" onclick = pendiente(conversor(${baseID}))><h2><<<<< Volver al inicio <<<</h2></a>

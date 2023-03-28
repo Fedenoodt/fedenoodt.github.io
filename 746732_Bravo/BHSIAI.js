a657842b93276d48e7510b3a348eb9db45f91280
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-03-28 16:22:52 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -127,7 +127,7 @@ let cabezal = `
 				<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "notGood">Receso de urgencia: 1/1 [ 07-03-2023 ]</p></a></div></li>
 				<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/ProyectoRayonnage" target="_blank"><p class = "good">Hacer las 20 herramientas del Proyecto Rayonnage: 5/20 </p></a></div></li>
 				<li class="listaHorizontal"><div><a href="" target="_blank"><p class = "good">Encontrar la manera de Ejecutar Empire at War Gold, y Resident Evil 4 en una m&aacute;quina de TecniBot. </p></a></div></li>
-				<li class="listaHorizontal"><div><a href="" target="_blank"><p class = "good">Prepararse para ir con Marcelo Bivort por Engranaje Embarrado el 28-03 a las 18hs. </p></a></div></li>
+				<li class="listaHorizontal"><div><a href="" target="_blank"><p class = "notGood">Prepararse para ir con Marcelo Bivort por Engranaje Embarrado el 28-03 a las 18hs. </p></a></div></li>
 				
                 </ul>
         </div>

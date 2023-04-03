cd20ca62cbf7f47e52d5ccba50012c774e90c556
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-04-03 19:15:26 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -126,7 +126,7 @@ let cabezal = `
                 <ul>
 				<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "good">Receso de urgencia: 0/3 [  ]</p></a></div></li>
 				<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/ProyectoRayonnage" target="_blank"><p class = "good">Hacer las 20 herramientas del Proyecto Rayonnage: 5/20 </p></a></div></li>
-				<li class="listaHorizontal"><div><a href="" target="_blank"><p class = "good">Estar listo para ir con Marcelo Bivort por Engranaje Embarrado los Martes a las 18hs. </p></a></div></li>
+				<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html" target="_blank"><p class = "good">Estar listo para ir con Marcelo Bivort por Engranaje Embarrado los Martes a las 18hs. </p></a></div></li>
 				
                 </ul>
         </div>

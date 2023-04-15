d6f783b900588d9b2fcb3c6474621ae525492ca1
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-04-15 00:42:33 -0300

    Update odtu.js

--- Pagina_personal/ocio/odtu.js
+++ Pagina_personal/ocio/odtu.js
@@ -16,9 +16,9 @@ const onlineSERV = '<h3 class="online">El servicio está online.</h3>';
 const offlineSERV = '<h3 class="online">El servicio está offline.</h3>';
 
 
-const lan = '53731';
+const lan = '55281';
 
-const active = '0';
+const active = '1';
 
 const status = online;
 

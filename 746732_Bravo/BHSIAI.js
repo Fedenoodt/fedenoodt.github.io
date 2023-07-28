d09a17441b0662a6c30fcf3ae1f47fd9f18b4191
Fedenoodt <56651406+Fedenoodt@users.noreply.github.com>
2023-07-28 20:47:45 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -463,13 +463,14 @@ function streaming () {
     <div class="in-flex">
 			<h2>Streaming</h2>
 			<ul>
+				<li><a href="https://www.primevideo.com/?ref_=dvm_pds_amz_ar_dc_s_g_mkw_sURBO2uG4-dc_pcrid_231825120471&mrntrk=slid__pgrid_42306243048_pgeo_1000073_x__ptid_kwd-45697133742" target="_blank">Amazon Prime Video</a>></li>
 				<li><a href="https://web.flow.com.ar/guia-de-tv?epg" target="_blank">Cablevision Flow</a>></li>
-				<li><a href="https://www.netflix.com/browse" target="_blank">Netflix</a>></li>
-				<li><a href="https://www.netflix.com/browse/genre/75436?bc=34399&so=yr" target="_blank">Netflix - Policiales ordenadas por a&ntilde;o</a>></li>
 				<li><a href="https://www.disneyplus.com/es-419/home" target="_blank">Disney+</a>></li>
 				<li><a href="https://play.hbomax.com/" target="_blank">HBO Max</a>></li>
+				<li><a href="https://kick.com/" target="_blank">Kick</a>></li>
+				<li><a href="https://www.netflix.com/browse" target="_blank">Netflix</a>></li>
+				<li><a href="https://www.netflix.com/browse/genre/75436?bc=34399&so=yr" target="_blank">Netflix - Policiales ordenadas por a&ntilde;o</a>></li>
 				<li><a href="https://www.starplus.com/es-419/home" target="_blank">Star+</a>></li>
-				<li><a href="https://www.primevideo.com/?ref_=dvm_pds_amz_ar_dc_s_g_mkw_sURBO2uG4-dc_pcrid_231825120471&mrntrk=slid__pgrid_42306243048_pgeo_1000073_x__ptid_kwd-45697133742" target="_blank">Amazon Prime Video</a>></li>
 
 				<li></a><a href = "" onclick = pendiente(conversor(${mediosID}))>Volver</a><<</li>
 			</ul>
@@ -560,6 +561,7 @@ function redes () {
 				<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAI.html"><a href="https://www.facebook.com/" target="_blank">Facebook</a></a>></li>
 				<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAI.html"><a href="https://www.instagram.com/?hl=es-la" target="_blank">Instagram</a></a>></li>
 				<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAI.html"><a href="https://twitter.com/home" target="_blank">Twitter</a></a>></li>
+				<li><a href="https://fedenoodt.github.io/798326_Epsilon/BHSIAI.html"><a href="https://www.tiktok.com/foryou" target="_blank">TikTok</a></a>></li>
 				<li><a href = "" onclick = pendiente(conversor(${baseID}))>Volver</a><<</li>
 			</ul>
 		</div>

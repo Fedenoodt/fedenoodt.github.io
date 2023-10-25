328e1b559db778981ee33ae6c83a8b546111aa06
Fedenoodt <fedenoodtmolins@gmail.com>
2023-10-25 10:00:51 -0300

    Update dos.js

--- Pagina_personal/areaPruebas/sector1/dos.js
+++ Pagina_personal/areaPruebas/sector1/dos.js
@@ -1,4 +1,4 @@
 fetch('https://dolarapi.com/v1/dolares/blue')
-.then((data) => data.json())
+.then(const data = data.json())
 .then(console.log(data))
 .catch(console.error('No se ha podido extraer informaciòn del dolar'))

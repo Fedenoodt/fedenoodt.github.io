9dc8026e1f9d556e7209c0460c1bd45ce0e6fb1f
Fedenoodt <fedenoodtmolins@gmail.com>
2023-12-17 16:21:39 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -127,7 +127,7 @@ let cabezal = `
 	            <br />
 	            <h1>Black Hawk Security Informatics Anonymous Incorporation</h1>
 	            <hr />	
-	            <h2> ~ Versión 45.8 ~ </h2>
+	            <h2> ~ Versión 46.2 ~ </h2>
 	            <hr />	
 	                <ul>
 					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "notGood">Receso de urgencia: 1/1 [ 04 - 12 - 2023 ]</p></a></div></li>

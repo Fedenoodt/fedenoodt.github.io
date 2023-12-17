35ab66926f54aaa105f498a5f24a0da1d6b936cd
Fedenoodt <fedenoodtmolins@gmail.com>
2023-12-17 16:03:40 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -513,6 +513,7 @@ function streaming () {
 					<ul>
 						<li><a href="https://www.primevideo.com/?ref_=dvm_pds_amz_ar_dc_s_g_mkw_sURBO2uG4-dc_pcrid_231825120471&mrntrk=slid__pgrid_42306243048_pgeo_1000073_x__ptid_kwd-45697133742" target="_blank">Amazon Prime Video</a>></li>
 						<li><a href="https://web.flow.com.ar/guia-de-tv?epg" target="_blank">Cablevision Flow</a>></li>
+						<li><a href="https://www.disneyplus.com/es-419/brand/star-wars" target="_blank">Disney+ - Star Wars</a>></li>
 						<li><a href="https://www.disneyplus.com/es-419/home" target="_blank">Disney+</a>></li>
 						<li><a href="https://play.hbomax.com/" target="_blank">HBO Max</a>></li>
 						<li><a href="https://kick.com/" target="_blank">Kick</a>></li>

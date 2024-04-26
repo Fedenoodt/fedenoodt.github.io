456b5f6019e68b600b01f396f9133949897dbf3b
Fedenoodt <fedenoodtmolins@gmail.com>
2024-04-26 19:11:01 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -130,10 +130,9 @@ let cabezal = `
 	            <h2> ~ Versión 46.6 ~ </h2>
 	            <hr />	
 	                <ul>
-					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "notGood">Receso de urgencia: 1/1 [ 13 - 02 - 2024 ]</p></a></div></li>
-					<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/Trabajo/edit/main/Proyecto%20Zonda/README.md" target="_blank"><p class = "notGood">Atender al sub Proyecto Zonda </p></a></div></li>
+					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "good">Receso de urgencia: -/- [  -  -  ]</p></a></div></li>
+					<li class="listaHorizontal"><div><a href="https://app.diagrams.net/#G1zpCxzziDJtjfn7NgvhpWNLnBwTFlcvxZ#%7B%22pageId%22%3A%22ckcK2c53pp8Jp9gSL_0J%22%7D" target="_blank"><p class = "good">Mejora técnologica: Atender asuntos de Proyecto TecniBot </p></a></div></li>
 					<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/ProyectoRayonnage" target="_blank"><p class = "good">Hacer las 20 herramientas del Proyecto Rayonnage: 7/20 </p></a></div></li>
-					<li class="listaHorizontal"><div><a href="https://cursos.utnba.centrodeelearning.com/my/courses.php" target="_blank"><p class = "good">Estar atento a la clase de la UTN a las 20hs de un lunes. </p></a></div></li>
 					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoZero.html" target="_blank"><p class = "good">Estar listo para ir con Marcelo Bivort por el Proyecto Zero, Martes por medio a las 18hs. </p></a></div></li>
 					
 	                </ul>

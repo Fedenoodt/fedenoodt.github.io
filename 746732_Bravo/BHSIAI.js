23e625c5e4f138924dd38f8db850ab5a60f039bb
Fedenoodt <fedenoodtmolins@gmail.com>
2025-02-17 13:55:28 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -133,8 +133,6 @@ let cabezal = `
 	                <ul>
 					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoEE.html#nota5" target="_blank"><p class = "good">Receso de urgencia: -/- [    -  -  ]</p></a></div></li>
 					<li class="listaHorizontal"><div><a href="https://app.diagrams.net/#G109ytHwAWI3js3y8z3QKzk1i1Y7eDIUD6#%7B%22pageId%22%3A%22ueUZChO0UBstpgEd6729%22%7D" target="_blank"><p class = "good">Mejora tecnológica: Responder a las demandas de Zonda por Zero. </p></a></div></li>
-					<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/fedenoodt.github.io/blob/gh-pages/Pagina_personal/PyC/anexat-app/PseudoAlgoritmo.txt" target="_blank"><p class = "notGood">Completar Proyecto Anexat </p></a></div></li>
-					<li class="listaHorizontal"><div><a href="https://github.com/Fedenoodt/fedenoodt.github.io/tree/gh-pages/Pagina_personal/vida/moneda/Monetizer" target="_blank"><p class = "notGood">Completar Proyecto Monetizer </p></a></div></li>
 					<li class="listaHorizontal"><div><a href="https://fedenoodt.github.io/Pagina_personal/PyC/proyectoZero.html" target="_blank"><p class = "good">Estar listo para ir con Marcelo Bivort por el Proyecto Zero, Martes por medio a las 18hs. </p></a></div></li>
 					
 	                </ul>

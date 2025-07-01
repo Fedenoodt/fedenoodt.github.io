b44e6a4837b2c7ac4a72fba49aba12048581f4f0
Fedenoodt <fedenoodtmolins@gmail.com>
2025-07-01 02:10:28 -0300

    Update BHSIAI.js

--- 746732_Bravo/BHSIAI.js
+++ 746732_Bravo/BHSIAI.js
@@ -561,13 +561,14 @@ function streaming () {
 return imagen
 }
 
-function juegos () {
+function juegos () {    
     let cuerpo = document.createElement("div");
     const imagen = `
     ${cabezal}
 		    <div>
 				<h2>Juegos</h2>
 					<ul>
+						<li><a href="fedenoodt.github.io/Pagina_personal/ocio/Minecraft/Cardinales/Bosque_Susurros/index.html" target="_blank">Minecraft cardnales: Bosque de los Susurros</a>></li>
 						<li><a href = "" onclick = pendiente(conversor(${minecraftID}))>Minecraft (Webs de informaci&oacute;n)</a>></li>
 						<li><a href="https://flexboxfroggy.com/#es" target="_blank">FLEXBOX FROGGY</a>></li>
 						<li><a href="https://agar.io" target="_blank">Agar.io</a>></li>

88bacffd386f7b9326cfe6df1d14e3c2103ebc44
Fedenoodt <fedenoodtmolins@gmail.com>
2025-08-15 23:49:14 -0300

    Update script.js

--- Pagina_personal/ocio/Randomero Simpsons/script.js
+++ Pagina_personal/ocio/Randomero Simpsons/script.js
@@ -1,7 +1,7 @@
 function eject () {
     // La constante temporadas, es la que mantiene el índice de temporadas al día como clave, y la cantidad de capítulos de ella, como valor.
     const temporadas = { 1 : 13, 2 : 22, 3 : 23, 4 : 22, 5 : 22, 6 : 25, 7 : 25, 8 : 25, 9 : 25, 10 : 23, 11 : 22, 
-        12 : 21, 13 : 22, 14 : 22, 15 : 22, 16 : 21, 17 : 22, 18 : 22, 19 : 20, 20 : 21, 21 : 23, 22 : 22, 23 : 22, 
+        12 : 21, 13 : 22, 14 : 15, 15 : 22, 16 : 21, 17 : 22, 18 : 22, 19 : 20, 20 : 21, 21 : 23, 22 : 22, 23 : 22, 
         23 : 22, 24 : 22, 25 : 22, 26 : 22, 27 : 22, 28 : 21, 29 : 21, 30 : 23, 31 : 22, 32 : 22, 33 : 21, 34 : 22, 
         35 : 18, 36 : 11
     };
@@ -84,3 +84,4 @@ function ajustarFondo() {
 window.addEventListener("resize", ajustarFondo);
 // ... y DOMContentLoaded para asegurarnos que todo DOM este listo.
 document.addEventListener("DOMContentLoaded", ajustarFondo);
+
